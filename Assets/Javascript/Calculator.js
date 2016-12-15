8489b25711763b6d61f4d074027f8477c6eb22a1
Tim Van den Enden <99038115@mydavinci.nl>
2016-12-15 12:12:13 +0100

    new version 4.0 js :)

--- Assets/Javascript/Calculator.js
+++ Assets/Javascript/Calculator.js
@@ -1,4 +1,4 @@
-// Version 3.0 made by T1mINC \\
+// Version 4.0 made by T1mINC
 var Calculator = {};
 var operators = [];
 var numbers = [];
@@ -46,34 +46,82 @@ Calculator.calculate = function() {
 		numbers[0] = numbers[1];
 		numbers[1] = b;
 	}
-
-	while(operators.includes("*")) {
-		var pos = operators.indexOf("*");
-		numbers[pos] = Number(numbers[pos]) * Number(numbers[pos + 1]);
-		operators.splice(pos, 1);
-		numbers.splice(pos + 1, 1);
+	while ((operators.includes("*") || operators.includes("/"))) {
+		// Dit gebeurt alleen als er een * of / In de som zit.
+		var keer = operators.indexOf("*");
+		var delen = operators.indexOf("/");
+	
+		var position  = -1;
+	
+		if (((keer < delen) && keer > -1) || (keer >= 0 && delen == -1)) {
+			position = keer;
+		} else {
+			if (delen > -1){
+				position = delen;
+			}
+		}
+	
+		if (operators[position] == "*") {
+			numbers[position] = Number(numbers[position]) * Number(numbers[position + 1]);
+		} else {
+			numbers[position] = Number(numbers[position]) / Number(numbers[position + 1]);
+		}
+	
+		operators.splice(position, 1);
+		numbers.splice(position + 1, 1);
 	}
-
-	while(operators.includes("/")) {
-		var pos = operators.indexOf("/");
-		numbers[pos] = Number(numbers[pos]) / Number(numbers[pos + 1]);
-		operators.splice(pos, 1);
-		numbers.splice(pos + 1, 1);
+	
+	while ((operators.includes("+") || operators.includes("-"))) {
+		// Dit gebeurt alleen als er een * of / In de som zit.
+		var plus = operators.indexOf("+");
+		var min = operators.indexOf("-");
+	
+		var position  = -1;
+	
+		if (((plus < min) && plus > -1) || (plus >= 0 && min == -1)) {
+			position = plus;
+		} else {
+			if (min > -1){
+				position = min;
+			}
+		}
+	
+		if (operators[position] == "+") {
+			numbers[position] = Number(numbers[position]) + Number(numbers[position + 1]);
+		} else {
+			numbers[position] = Number(numbers[position]) - Number(numbers[position + 1]);
+		}
+	
+		operators.splice(position, 1);
+		numbers.splice(position + 1, 1);
 	}
+	// while(operators.includes("*")) {
+	// 	var pos = operators.indexOf("*");
+	// 	numbers[pos] = Number(numbers[pos]) * Number(numbers[pos + 1]);
+	// 	operators.splice(pos, 1);
+	// 	numbers.splice(pos + 1, 1);
+	// }
 
-	while(operators.includes("+")) {
-		var pos = operators.indexOf("+");
-		numbers[pos] = Number(numbers[pos]) + Number(numbers[pos + 1]);
-		operators.splice(pos, 1);
-		numbers.splice(pos + 1, 1);
-	}
+	// while(operators.includes("/")) {
+	// 	var pos = operators.indexOf("/");
+	// 	numbers[pos] = Number(numbers[pos]) / Number(numbers[pos + 1]);
+	// 	operators.splice(pos, 1);
+	// 	numbers.splice(pos + 1, 1);
+	// }
 
-	while(operators.includes("-")) {
-		var pos = operators.indexOf("-");
-		numbers[pos] = Number(numbers[pos]) - Number(numbers[pos + 1]);
-		operators.splice(pos, 1);
-		numbers.splice(pos + 1, 1);
-	}
+	// while(operators.includes("+")) {
+	// 	var pos = operators.indexOf("+");
+	// 	numbers[pos] = Number(numbers[pos]) + Number(numbers[pos + 1]);
+	// 	operators.splice(pos, 1);
+	// 	numbers.splice(pos + 1, 1);
+	// }
+
+	// while(operators.includes("-")) {
+	// 	var pos = operators.indexOf("-");
+	// 	numbers[pos] = Number(numbers[pos]) - Number(numbers[pos + 1]);
+	// 	operators.splice(pos, 1);
+	// 	numbers.splice(pos + 1, 1);
+	// }
 
 	anwser = numbers[0];
 	// For SomDisplay Update! And AnwserDisplay!
